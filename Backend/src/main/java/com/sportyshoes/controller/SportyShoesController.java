481ecef231647c9a41a06009f8224e02f2a07d9e
MounishP <mnsh.pv1@gmail.com>
2022-11-05 22:48:34 +0530

    Initial commit

---
+++ Backend/src/main/java/com/sportyshoes/controller/SportyShoesController.java
@@ -0,0 +1,7 @@
+package com.sportyshoes.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SportyShoesController {
+}
---
+++ Backend/src/main/java/com/sportyshoes/model/AdminReq.java
@@ -0,0 +1,7 @@
+package com.sportyshoes.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AdminReq {
+}
